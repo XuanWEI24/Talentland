f6f522cf1afabe7a5999cc6d809bfa230858915a
XuanWEI24 <60135250+XuanWEI24@users.noreply.github.com>
2020-04-16 03:57:44 +0800

    Add files via upload

---
+++ 2-CreerUnBesoin/CreerVotreBesoin.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+​
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+​	<link href="/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>CREER UN BESOIN</title>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+	  .inputEmail{ width: 3%;
+	  }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+  </head>
+  <body>
+        <nav class="navbar navbar-dark bg-dark">
+          <a class="navbar-brand" href="Accueil.html"> Quai des savoir-faire </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+              
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Accueil <span class="sr-only">(lien courant)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Besoins</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Talents</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Plus
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Ateliers</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Projet associatif</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Vie ma vie</a>
+                </div>
+              </li>
+            </ul>
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Login
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Mon espace</a>
+                <a class="dropdown-item" href="#">Mes demandes</a>
+                <a class="dropdown-item" href="#">Mes offres</a>
+                <a class="dropdown-item" href="#">Déconnecter</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+		
+		<!--------------------------------------------------------------------------------------------------------------------------------------------->
+        <div class="jumbotron">
+          <div class="container">
+			
+			<h1> CREER UN BESOIN </h1>
+			<form action="Saisir1Besoin.php" method="post">
+                        <?php
+			echo "Date de création : " . date("d/m/yy"); 
+			?>
+			<div class="form-row align-items-center">
+				<div class="col-auto my-1">
+				  <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                                  <select class="custom-select mr-sm-2" name="categorie" id="inlineFormCustomSelect" required>
+					<option selected>Choose une catégorie </option>
+					<option value="1" title="...">Sport</option>
+                                        <option value="2" title="Réunions créatives/Pitcher .....">Animation</option>
+					<option value="3" title="...">Outils métiers</option>
+                                        <option value="4" title="Yoga, méditation...">Développement personnel</option>
+					<option value="5" title="...">Associatif</option>
+                                        <option value="6" title="...">Covoiturage</option>
+					<option value="7" title="Word, Excel, PowerPoint, Outlook...">Bureautique</option>
+					<option value="8" title="Internet, site Web, réparation PC...">Informatique</option>
+					<option value="9" title="Cuisine, bricolage, musique, théâtre, ciné, culture, philatélie, généalogie...">Loisir </option>
+					<option value="10" title="Demande de créér une catégorie à l'administrateur">Autres </option>
+				  </select>
+				</div>
+			</div>
+				<div class="form-group">
+				  <label for="inputEmail4">Titre</label>
+				  <input type="text" name="titre" class="form-control" id="inputEmail4" maxlength="40" required>
+				</div>
+				<div class="form-group">
+					<label for="inputEmail4">Déscription du besoin</label><br/>
+					<textarea rows="4" cols="50" name="description" required> Veuillez préciser votre besoin</textarea>
+				</div>
+				<div class="form-group">
+				  <label for="inputEmail4">Date butoire</label>
+                                  <input type="date" name="datebutoire" class="form-control" id="inputEmail4" maxlength="10" required>
+				</div>
+			  <div class="form-group">
+				<label for="inputAddress">Type de besoin</label>				
+			  </div>
+			  <div class="form-group">
+				  <div class="form-check form-check-inline">
+					  <input class="form-check-input" type="checkbox" name="pro" id="inlineCheckbox1" value="Pro">
+					  <label class="form-check-label" for="inlineCheckbox1">Pro</label>
+					</div>
+					<div class="form-check form-check-inline">
+					  <input class="form-check-input" type="checkbox" name="perso" id="inlineCheckbox2" value="Perso">
+					  <label class="form-check-label" for="inlineCheckbox2">Perso</label>
+					</div>
+				  </div>
+			  </div>
+			  <div class="form-group">
+			  <button type="submit" class="btn btn-primary">CREER</button>
+			  </div>
+            </form>
+          </div>
+        </div>
+          <footer>
+            <p id="copyright"><em><small>copyright &#9400; Talents Land, CPAM Haute-Garonne, 2020. All rights reserved.</small></em></p>
+          </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
