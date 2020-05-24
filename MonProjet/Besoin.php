17860d987945b002548d2aebdaf3a349e500d2a9
XuanWEI24 <60135250+XuanWEI24@users.noreply.github.com>
2020-05-24 17:15:27 +0800

    Update Besoin.php

--- MonProjet/Besoin.php
+++ MonProjet/Besoin.php
@@ -74,27 +74,39 @@
             <a href="BesoinC.php"><div class="alert alert-light" role="alert">
               Filtrer les besoins par catégorie
             </div></a>
+		  
+            <form method="GET" class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" name="mot" placeholder="Excel ..." aria-label="Search">
+                        <button type="submit" class="btn btn-outline-dark" value="Valider">Recherche</button>
+            </form> 
             
             <div class="flex-parent d-flex flex-wrap justify-content-around mt-3">
-            <?php
-            require_once('Fonctions.php');
-            $query = "select b.TitreB, c.PhotoC, b.DateButoireB from besoins b, categories c where b.CodeC = c.CodeC order by CodeB DESC";
-            $result = mysqli_query ($session, $query);
+		    <?php
+			    require_once('Fonctions.php');
+			    $query = "select b.TitreB, c.PhotoC, b.DateButoireB from besoins b, categories c where b.CodeC = c.CodeC order by CodeB DESC";
 
-            if ($result == false) {
-                die("ereur requête : ". mysqli_error($session) );
-            }
-            while ($ligne = mysqli_fetch_array($result)) {                      /* Afficher tous les besoins par l'ordre chronologique en format carte */
-                echo ('<div class="card" style="width: 12rem;">');
-                echo ('<img src="'.$ligne["PhotoC"].'" class="card-img-top" alt="...">');   
-                echo ('<div class="card-body card text-center">');
-                echo ('<h5 class="card-title">'.$ligne["TitreB"].'</h5>');
-                echo ('<p class="card-text">Délais souhaité: '.$ligne["DateButoireB"].'</p>');
-                echo ('<a href="BesoinX.php" class="btn btn-outline-dark">Voir la demande</a>'); 
-                echo ('</div>');  
-                echo ('</div>');               
-            }                
-            ?>
+			    if(isset($_GET['mot']) AND !empty($_GET['mot'])) {     /*Recherche par mot clé*/
+				$mot = htmlspecialchars($_GET['mot']);
+				$query = "select b.TitreB, c.PhotoC, b.DateButoireB from besoins b, categories c where b.CodeC = c.CodeC and b.TitreB LIKE '%$mot%' order by b.CodeB DESC";
+			    }
+
+			    $result = mysqli_query ($session, $query);
+
+			    if (mysqli_num_rows($result)>0) {
+				while ($ligne = mysqli_fetch_array($result)) {                      /* Afficher tous les besoins par l'ordre chronologique en format carte */
+				echo ('<div class="card" style="width: 12rem;">');
+				echo ('<img src="'.$ligne["PhotoC"].'" class="card-img-top" alt="...">');   
+				echo ('<div class="card-body card text-center">');
+				echo ('<h5 class="card-title">'.$ligne["TitreB"].'</h5>');
+				echo ('<p class="card-text">Délais souhaité: '.$ligne["DateButoireB"].'</p>');
+				echo ('<a href="BesoinX.php" class="btn btn-outline-dark">Voir la demande</a>'); 
+				echo ('</div>');  
+				echo ('</div>');         
+				}
+			    } else {
+			      echo('<h5> Aucun résultat pour : '.$mot.'</h5>');
+			    }        
+			?>
             </div>
           </div>
         </div>
