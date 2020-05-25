14fa6ca96ef98e72c3f850a72aeff82b9f3f0d89
XuanWEI24 <60135250+XuanWEI24@users.noreply.github.com>
2020-05-25 15:47:18 +0800

    Update Talent.php

--- MonProjet/Talent.php
+++ MonProjet/Talent.php
@@ -74,26 +74,38 @@
             <a href="TalentC.php"><div class="alert alert-light" role="alert">
               Filtrer les talents par catégorie
             </div></a>
+		  
+	    <form class="form-inline my-2 my-lg-0">
+            	<input class="form-control mr-sm-2" type="search" placeholder="Guitare ..." aria-label="Search">
+            	<button type="button" class="btn btn-outline-dark">Recherche</button>
+            </form>  
             
             <div class="flex-parent d-flex flex-wrap justify-content-around mt-3">
             <?php
-            require_once('Fonctions.php');
+		    require_once('Fonctions.php');
+            	    $query = "select t.TitreT, c.PhotoC from talents t, categories c where t.CodeC = c.CodeC order by t.CodeT DESC";
 
-            $query = "select t.TitreT, c.PhotoC from talents t, categories c where t.CodeC = c.CodeC order by CodeT DESC";
-            $result = mysqli_query ($session, $query);
+                    if(isset($_GET['mot']) AND !empty($_GET['mot'])) {     /*Recherche par mot clé*/
+                            $mot = htmlspecialchars($_GET['mot']);
+                            $query = "select t.TitreT, c.PhotoC from talents t, categories c where t.CodeC = c.CodeC and t.TitreT LIKE '%$mot%' order by t.CodeT DESC";
+                    }
 
-            if ($result == false) {
-                die("ereur requête : ". mysqli_error($session) );
-            }
-            while ($ligne = mysqli_fetch_array($result)) {                      /* Afficher tous les talents par l'ordre chronologique */
-                echo ('<div class="card" style="width: 12rem;">');
-                echo ('<img src="'.$ligne["PhotoC"].'" class="card-img-top" alt="...">');   
-                echo ('<div class="card-body card text-center">');
-                echo ('<h5 class="card-title">'.$ligne["TitreT"].'</h5>');
-                echo ('<a href="TalentX.php" class="btn btn-outline-dark">Voir le détail</a>'); 
-                echo ('</div>');  
-                echo ('</div>');                       
-            }                
+                    $result = mysqli_query ($session, $query);
+
+                    if (mysqli_num_rows($result)>0) {
+                        while ($ligne = mysqli_fetch_array($result)) {                      /* Afficher tous les besoins par l'ordre chronologique en format carte */
+                        echo ('<div class="card" style="width: 12rem;">');
+                        echo ('<img src="'.$ligne["PhotoC"].'" class="card-img-top" alt="...">');   
+                        echo ('<div class="card-body card text-center">');
+                        echo ('<h5 class="card-title">'.$ligne["TitreB"].'</h5>');
+                        echo ('<p class="card-text">Délais souhaité: '.$ligne["DateButoireB"].'</p>');
+                        echo ('<a href="BesoinX.php" class="btn btn-outline-dark">Voir la demande</a>'); 
+                        echo ('</div>'); 
+		        echo ('</div>');         
+                        }
+                     } else {
+                        echo('<h5> Aucun résultat pour : '.$mot.'</h5>');
+                     }                                         
             ?>
             </div>
           </div>
